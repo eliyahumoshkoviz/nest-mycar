0d8bc8e36dbbf7eb5f4ac8c3b8ddcaad23ee7d59
= <=>
2024-08-14 16:56:19 +0300

    added findUserByCookie path

--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -2,6 +2,7 @@ import { Expose } from "class-transformer";
 
 export class UserDto {
     
+    //by expose Even if there are more fields return only this field use with serializer
     @Expose()
     id:number;
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Param, Query, Delete, Patch, NotFoundException } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, Query, Delete, Patch, NotFoundException, Session } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto copy';
@@ -12,18 +12,28 @@ export class UsersController {
   constructor(private usersService: UsersService, private authService: AuthService) { }
 
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user
   }
 
   @Post('/signin')
-  signin(@Body() body: CreateUserDto) {    
-    return this.authService.signin(body.email, body.password);
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user
   }
 
-  @Get('/:id')
+  @Get('/single/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id))
+    if (!user) throw new NotFoundException('User not found');
+    return user
+  }
+  @Get('findUserByCookie')
+  async findUserByCookie(@Session() session: any) {    
+    const user = await this.usersService.findOne(session.userId)
     if (!user) throw new NotFoundException('User not found');
     return user
   }
